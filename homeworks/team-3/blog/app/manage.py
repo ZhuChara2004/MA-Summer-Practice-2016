a74a15165196e0a4ce1623ac5e55fc25bbee6028
serh <sergeyskotnitskiy@gmail.com>
2016-07-14 19:33:50 +0300

    little fix

---
+++ homeworks/team-3/blog/app/manage.py
@@ -0,0 +1,17 @@
+import os
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+
+from blog import blog, db
+
+
+app.config.from_object(os.environ['APP_SETTINGS'])
+
+migrate = Migrate(blog, db)
+manager = Manager(blog)
+
+manager.add_command('db', MigrateCommand)
+
+
+if __name__ == '__main__':
+    manager.run()
