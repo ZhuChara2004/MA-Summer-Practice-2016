e73099707827a15338e44caa26786e6fcba7a5a8
davygora <glory-gerrard@ukr.net>
2016-07-14 19:20:54 +0300

    Created model Comment

---
+++ homeworks/team-3/blog/app/comment.py
@@ -0,0 +1,18 @@
+from app import db
+from datetime import datetime
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(500), required=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __init__(self, body, created_at):
+        self.body = body
+        self.created_at = created_at
+
+
+        def __repr__(self):
+            return '<Comment %r>' % self.body
