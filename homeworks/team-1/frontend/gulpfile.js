69777642e5bfe6be85bdb7bf01765c9ef60f27eb
prolop <malamuzh@mail.ru>
2016-07-15 17:50:39 +0300

    gulptask

---
+++ homeworks/team-1/frontend/gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	cssnano = require('gulp-cssnano'),
+	autoprefixer = require('autoprefixer'),
+	postcss = require('gulp-postcss'),
+	rename = require('gulp-rename'),
+	del = require('del');
+
+gulp.task('css', function() {
+	var processors = [
+		autoprefixer({browsers: ['last 15 versions', '> 1%', 'ie 8', 'ie 7']}),
+	];
+
+	return gulp.src('app/sass/**/*.sass')
+		.pipe(sass())		
+		.pipe(postcss(processors))
+		.pipe(gulp.dest('app/css'));
+});
+
+gulp.task('min', ['css'], function() {
+	return gulp.src('app/css/main.css')
+	.pipe(cssnano())
+	.pipe(rename({suffix: '.min'}))
+	.pipe(gulp.dest('app/css'))
+})
+
+gulp.task('watch', ['min'], function() {
+	gulp.watch('app/sass/**/*.sass', ['css', 'min']);
+});
+
+gulp.task('clean', function() {
+	return del.sync('dist')
+})
+
+gulp.task('build' , ['clean', 'css', 'min'], function() {
+	var buildCss = gulp.src([
+		'app/css/main.css',
+		'app/css/main.min.css'
+	])
+
+	.pipe(gulp.dest('dist/css'))
+})
