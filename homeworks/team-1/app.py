199e4428d58b4bd3d7a479ce9d6fecc53220100d
Yurik Abdulayev <yurs11k89@gmail.com>
2016-07-16 14:07:51 +0300

    return all posts in index

--- homeworks/team-1/app.py
+++ homeworks/team-1/app.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from db import db_all
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    list_posts = list(reversed(db_all()))
+    return render_template('index.html', posts=list_posts)
 
 
-if "__name__" == "__main__":
+if __name__ == '__main__':
     app.run()
+
