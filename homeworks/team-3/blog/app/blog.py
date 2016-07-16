f9880ce35635200bc628f17a29212f5be33d4ee9
serh <sergeyskotnitskiy@gmail.com>
2016-07-16 19:16:16 +0300

    added look post by id

--- homeworks/team-3/blog/app/blog.py
+++ homeworks/team-3/blog/app/blog.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, request, redirect, url_for
 from flask import render_template
-from crud import post_all
+from crud import post_all, post_look
 
 blog = Flask(__name__)
 blog.config.from_object(os.environ['APP_SETTINGS'])
@@ -13,6 +13,12 @@ from database import db_session
 def index():
     posts = post_all(db_session)
     return render_template('index.html', posts=posts)
+
+
+@blog.route('/<id>', methods=['GET'])
+def look(id):
+    post = post_look(db_session, id)
+    return render_template('post.html', post=post)
 
 
 @blog.teardown_appcontext
--- homeworks/team-3/blog/app/crud.py
+++ homeworks/team-3/blog/app/crud.py
@@ -4,3 +4,7 @@ from sqlalchemy import desc
 
 def post_all(db):
     return db.query(Post).order_by(desc(Post.created_at)).all()
+
+
+def post_look(db, post_id):
+    return db.query(Post).filter(Post.id == post_id).first()
