c35f37381c2937ba64546516958539c4ed71d365
Yurik Abdulayev <yurs11k89@gmail.com>
2016-07-25 18:58:44 +0300

    create models

---
+++ homeworks/team-1/test_prof/app/models.py
@@ -0,0 +1,33 @@
+from test_prof.app.__init__ import db
+
+
+class Test(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name_test = db.Column(db.String(120))
+    questions_id = db.Column(db.Integer, db.ForeignKey('questions.id'))
+    questions = db.relationship('Test', backref=db.backref('tests', lazy='dynamic'))
+
+
+class Questions(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String(200))
+    answers_id = db.Column(db.Integer, db.ForeignKey('answers.id'))
+    answers = db.relationship('Answers', backref='question', lazy='dynamic')
+
+
+class Answers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    answer = db.Column(db.String(120))
+    # id_question = db.Column(db.Integer, db.ForeignKey('questions.id'))
+
+
+class Directions(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name_direction = db.Column(db.String(50))
+
+
+
+
+
+
+
