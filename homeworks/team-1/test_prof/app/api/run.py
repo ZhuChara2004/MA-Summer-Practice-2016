8348f4a9e587bbc0761b6118fc67f5b65abb72ec
Yurik Abdulayev <yurs11k89@gmail.com>
2016-08-03 22:29:00 +0300

    add view to select test

--- homeworks/team-1/test_prof/app/api/run.py
+++ homeworks/team-1/test_prof/app/api/run.py
@@ -4,10 +4,6 @@ from flask import render_template
 
 @app.route('/', methods=['GET'])
 def get_first():
-    # url = "http://127.0.0.1:5000/api/v1.0/tests"
-    # obj = urllib3.urlopen(url).read()
-    # data = json.loads(obj)
-    # print(data)
     return render_template('index.html')
 
 if __name__ == '__main__':
