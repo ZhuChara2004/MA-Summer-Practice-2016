ea4f018657ce489d4bf1241eec31d04bbef966fb
Yurik Abdulayev <yurs11k89@gmail.com>
2016-08-07 13:05:27 +0300

    add login admin , logout admin , create cookie

--- homeworks/team-1/test_prof/app/api/admin.py
+++ homeworks/team-1/test_prof/app/api/admin.py
@@ -5,3 +5,8 @@ token = "secret_token"
 
 def equals(t):
     return token == t
+
+
+def auth(usern, passw):
+    if usern == username and password == passw:
+        return token
--- homeworks/team-1/test_prof/app/api/run.py
+++ homeworks/team-1/test_prof/app/api/run.py
@@ -1,15 +1,44 @@
 from test_prof.app.api.__init__ import app
-from flask import render_template
+from flask import render_template, request, make_response, redirect
+from test_prof.app.api.admin import auth, equals
+import datetime
 
 
 @app.route('/', methods=['GET'])
-def get_first():
-    return render_template('index.html')
+def get_tests():
+    return render_template('test_index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def get_user():
+    if request.method == 'GET':
+        if equals(request.cookies.get('business_c_token')):
+            return render_template('admin.html')
+        else:
+            return render_template('login.html')
+    elif request.method == 'POST':
+        token = auth(request.form['login'], request.form['password'])
+        if token:
+            expire_date = datetime.datetime.now()
+            expire_date = expire_date + datetime.timedelta(days=365)
+            resp = make_response(redirect('/login'))
+            resp.set_cookie('business_c_token', value=token, expires=expire_date)
+            return resp
+        else:
+            return render_template('login.html', mesg='error validation')
+
+
+@app.route("/logout", methods=['GET'])
+def logout():
+    resp = make_response(redirect('/login'))
+    resp.set_cookie('business_c_token', '', expires=0)
+    return resp
 
 
 @app.route('/<id>', methods=['GET'])
 def test(id):
     return render_template("test.html", id=id)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
